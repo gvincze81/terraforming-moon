e1b8dd1f5ed2629cac944e46926451f3740dc385
Vincze Gábor <vinczegabor1981@gmail.com>
2022-03-02 10:54:14 +0100

    create instance

--- main.tf
+++ main.tf
@@ -5,3 +5,10 @@ provider "aws" {
 resource "aws_s3_bucket" "mars" {
   bucket = "vg-bucket"
 }
+
+resource "aws_instance" "project-mars" {
+  ami = "ami-0ca05c6eaa4ac40e0"
+  instance_type = "t2.micro"
+  security_groups = ["my-sg"]
+  key_name = "oregon"
+}
